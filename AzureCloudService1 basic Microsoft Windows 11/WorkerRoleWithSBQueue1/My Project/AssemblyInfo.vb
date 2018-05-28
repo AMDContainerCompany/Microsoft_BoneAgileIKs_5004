b6a5f44ba92fb5ced03f11383b544fa38a4260e4
NataliaVodianova <39686420+NataliaVodianova@users.noreply.github.com>
2018-05-29 07:52:38 +0900

    Internet DO
    えらー無し,.

---
+++ AzureCloudService1 basic Microsoft Windows 11/WorkerRoleWithSBQueue1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般的な情報は次の 
+' 一連の属性によって制御されます。これらの属性値を変更して
+' アセンブリに関連付けされた情報を修正します。
+
+' アセンブリの属性値を確認します
+
+<Assembly: AssemblyTitle("WorkerRoleWithSBQueue1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WorkerRoleWithSBQueue1")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+' 次の GUID は、このプロジェクトが COM に公開された場合の typelib の ID です
+<Assembly: Guid("d096307f-431b-4304-bdb8-4007886485f4")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています。
+'
+'      メジャー バージョン
+'      マイナー バージョン 
+'      ビルド番号
+'      リビジョン
+'
+' すべての値を指定することも、下記のように ‘*’ を使用することでビルドとリビジョン番号の 
+' 既定値を使用することもできます。
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ AzureCloudService1 basic Microsoft Windows 11/WorkerRoleWithSBQueue1/WorkerRole.vb
@@ -0,0 +1,148 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Linq
+Imports System.Net
+Imports System.Threading
+Imports Microsoft.ServiceBus
+Imports Microsoft.ServiceBus.Messaging
+Imports Microsoft.WindowsAzure
+Imports Microsoft.WindowsAzure.ServiceRuntime
+
+Public Class WorkerRole
+    Inherits RoleEntryPoint
+
+    ' キューの名前
+    Const QueueName As String = "ProcessingQueue"
+
+    ' QueueClient はスレッド セーフです。要求ごとに QueueClient を再作成するのではなく、 
+    ' キャッシュに保存することをお勧めします
+    Dim Client As QueueClient
+    Dim CompletedEvent = New ManualResetEvent(False)
+
+    Public Overrides Sub Run()
+
+        Trace.WriteLine("メッセージの処理を開始しています")
+
+        ' メッセージ ポンプを開始すると、受信した各メッセージに対してコールバックが呼び出され、クライアントがポンプを停止すると呼び出しが閉じられます。
+        Client.OnMessage(Sub(receivedMessage As BrokeredMessage)
+                             Try
+                                 ' メッセージを処理します
+                                 Trace.WriteLine("サービス バス メッセージを処理しています: " + receivedMessage.SequenceNumber.ToString())
+                             Catch
+                                 ' メッセージ処理固有の例外はここで処理します
+                             End Try
+                         End Sub)
+
+        CompletedEvent.WaitOne()
+
+    End Sub
+
+    Public Overrides Function OnStart() As Boolean
+
+        ' 同時接続の最大数を設定します 
+        ServicePointManager.DefaultConnectionLimit = 12
+
+        ' キューが存在しない場合は作成します
+        Dim connectionString As String = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString")
+        Dim namespaceManager As NamespaceManager = namespaceManager.CreateFromConnectionString(connectionString)
+        If (Not namespaceManager.QueueExists(QueueName)) Then
+            namespaceManager.CreateQueue(QueueName)
+        End If
+
+        ' キューを使用するクライアントを取得します
+        Client = QueueClient.CreateFromConnectionString(connectionString, QueueName)
+        Return MyBase.OnStart()
+
+    End Function
+
+    Public Overrides Sub OnStop()
+
+        ' Service Bus キューへの接続を閉じます
+        Client.Close()
+        CompletedEvent.Set()
+        MyBase.OnStop()
+
+    End Sub
+
+End Class
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
+
+Dim log As New EventLog("Application")
+Dim entries As New List(Of EventLogEntry)
+For Each entry As EventLogEntry In log.Entries
+If entry.Source = "Microsoft BoneAgileIKs 5004" Then
+entries.Add(entry)
+End If
+Next
